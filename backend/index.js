decb3ee998862da2bade8d984a18ac53061f34b9
Giancarlo <giancarlo.t0811@gmail.com>
2024-03-23 09:43:55 -0400

    actualizado

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,6 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware');
 const app = express();
 app.use(express.json({extended: true}));
 app.use(express.urlencoded({extended: true}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 app.use(cors({credentials: true, origin: "http://localhost:5173"}));
 
 app.use('/api/users', userRoutes);
